9b4411d53083edd4b7bf1669fd29b2db65cd4c3f
Omprakash Choudhary <omseervi098@gmail.com>
2023-02-21 05:12:25 +0530

    modified:   pages/about.js

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function About() {
   return (
     <Head>
-      <title>GreedyGame</title>
+      <title>GreedyGame | about</title>
       <meta name="description" content="from about.js" />
       <link rel="icon" href="/favicon.ico" />
       <meta property="og:description" content="from about.js" />
