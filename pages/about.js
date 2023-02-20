84f010bf5588c05c5650b9bdaa8e164584408fef
Omprakash Choudhary <omseervi098@gmail.com>
2023-02-21 05:08:36 +0530

    update

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+export default function About() {
+  return (
+    <Head>
+      <title>GreedyGame</title>
+      <meta name="description" content="from about.js" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:description" content="from about.js" />
+    </Head>
+  );
+}
